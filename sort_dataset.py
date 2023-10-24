8019651f4584eb5fdb51603237b12ebce02a3bc5
Mako <759506695@qq.com>
2023-10-24 17:34:17 +0800

    my init

---
+++ sort_dataset.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+import shutil
+
+"""从12611个训练集中获取1000张图片，并对少于10张的标签增强到10张，最终能够获得2000+图片"""
+
+
+def copyfile(fname, target_dir):
+    """将文件复制到指定文件夹"""
+    os.makedirs(target_dir, exist_ok=True)
+    shutil.copy(fname, target_dir)
+
+def sortDataset(data_dir, DF):
+    """创建新的数据集"""
+    for idx, row in DF.iterrows():
+        # 获取当前文件路径
+        filename = os.path.join(data_dir, 'train', f"{row['id']}.png")
+        # 获取该文件的标签
+        label = str(row['boneage'])
+        copyfile(filename, os.path.join('../../autodl-tmp', 'sorted_dataset', label))
+
+
+def reorg_aug_data(data_dir):
+    df = pd.read_csv(os.path.join(data_dir, 'train.csv'))
+    sortDataset(data_dir, df)
+
+if __name__ == '__main__':
+    data_dir = '../archive'
+    reorg_aug_data(data_dir=data_dir)
--- submitit_pretrain.py
+++ submitit_pretrain.py
@@ -19,8 +19,8 @@ import submitit
 def parse_args():
     trainer_parser = trainer.get_args_parser()
     parser = argparse.ArgumentParser("Submitit for MAE pretrain", parents=[trainer_parser])
-    parser.add_argument("--ngpus", default=8, type=int, help="Number of gpus to request on each node")
-    parser.add_argument("--nodes", default=2, type=int, help="Number of nodes to request")
+    parser.add_argument("--ngpus", default=1, type=int, help="Number of gpus to request on each node")
+    parser.add_argument("--nodes", default=1, type=int, help="Number of nodes to request")
     parser.add_argument("--timeout", default=4320, type=int, help="Duration of the job")
     parser.add_argument("--job_dir", default="", type=str, help="Job dir. Leave empty for automatic.")
 
@@ -31,9 +31,10 @@ def parse_args():
 
 
 def get_shared_folder() -> Path:
-    user = os.getenv("USER")
-    if Path("/checkpoint/").is_dir():
-        p = Path(f"/checkpoint/{user}/experiments")
+    # user = os.getenv("USER")
+    cur_file_path = Path("checkpoint").absolute()
+    if Path("./checkpoint/").is_dir():
+        p = Path(f"{cur_file_path}/experiments")
         p.mkdir(exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
